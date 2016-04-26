8b1f163f667e4812d8aef1da89b9dc464784f9f5
yiranyaoqiu <1491634755@qq.com>
2016-04-26 23:51:17 +0800

    Add the function to predicate the suscipious memory bug race.

---
+++ src/race/membug.cc
@@ -0,0 +1,337 @@
+#include "race/membug.h"
+#include "core/log.h"
+#include <fstream>
+
+namespace race
+{
+
+NullPtrDeref::NullPtrDeref()
+{}
+
+NullPtrDeref::~NullPtrDeref()
+{}
+
+bool NullPtrDeref::ValidPtrReadOrWrite(MemAccess *mem_acc)
+{
+	std::string file_name=mem_acc->inst->GetFileName();
+	int line=mem_acc->inst->GetLine();
+	// pointer write access must contains NULL
+	if(mem_acc->type==RACE_EVENT_WRITE)
+		return null_ptrwr_set_.find(FilenameAndLineHash(file_name.c_str(),
+			line))!=null_ptrwr_set_.end();
+	else
+		return null_ptrrd_set_.find(FilenameAndLineHash(file_name.c_str(),
+			line))==null_ptrrd_set_.end();
+}
+
+bool NullPtrDeref::Harmful(MemAccess *mem_acc1,MemAccess *mem_acc2)
+{
+	return ValidPtrReadOrWrite(mem_acc1) & ValidPtrReadOrWrite(mem_acc2);
+}
+
+// Load the information about the null pointer read
+bool NullPtrDeref::LoadStaticInfo(std::string &file_name)
+{
+	std::fstream in(file_name,std::ios::in);
+	if(!in)
+		return false;
+	/* Suspicious write null to the pointer and infeasible condition check
+	   and assignment read.
+	   @format filename +' '+ lineno +' '+ flag */
+	const char *delimit=" ",*fn,*ln,*fg;
+	fn=ln=fg=NULL;
+	char buffer[50];
+	while(!in.eof()) {
+		in.getline(buffer,50,'\n');
+		fn=strtok(buffer,delimit);
+		if(fn==NULL)
+			continue;
+		ln=strtok(NULL,delimit);
+		fg=strtok(NULL,delimit);
+
+		if(*fg=='0')
+			null_ptrwr_set_.insert(FilenameAndLineHash(fn,atoi(ln)));
+		else if(*fg=='1')
+			null_ptrrd_set_.insert(FilenameAndLineHash(fn,atoi(ln)));
+	}
+	in.close();
+	return true;
+}
+
+UnInitRead::UnInitRead():filter_(NULL)
+{}
+
+UnInitRead::~UnInitRead()
+{
+	delete filter_;
+	for(MemMetaInitTable::iterator iter=memmeta_init_table_.begin();
+		iter!=memmeta_init_table_.end();iter++) {
+		delete iter->second;
+	}
+}
+
+inline void UnInitRead::CreateGlobalRegionFilter(Mutex *lock)
+{
+	filter_=new RegionFilter(lock);
+}
+
+inline void UnInitRead::AddGlobalRegion(address_t region_start,size_t region_size)
+{
+	DEBUG_ASSERT(region_size);
+	filter_->AddRegion(region_start,region_size,false);
+}
+
+inline bool UnInitRead::InGlobalRegion(address_t addr)
+{
+	return filter_->Filter(addr,false);
+}
+
+bool UnInitRead::Harmful(MemMeta *meta,MemAccess *mem_acc1,MemAccess *mem_acc2)
+{
+	if(InGlobalRegion(meta->addr))
+		return false;
+	if(mem_acc1->type==RACE_EVENT_WRITE) {
+		if(HasWrite(meta,mem_acc1->thd_id))
+			return false;
+		SetInitWrite(meta,mem_acc1->thd_id);
+		return true;
+	}
+	else if(mem_acc2->type==RACE_EVENT_WRITE) {
+		if(HasWrite(meta,mem_acc2->thd_id))
+			return false;
+		SetInitWrite(meta,mem_acc2->thd_id);
+		return true;		
+	}
+}
+
+void UnInitRead::SetInitWrite(MemMeta *meta,thread_t thd_id)
+{
+	if(memmeta_init_table_.find(meta)==memmeta_init_table_.end())
+		memmeta_init_table_[meta]=new ThreadFlagMap;
+	ThreadFlagMap *thd_flg_map=memmeta_init_table_[meta];
+	if(thd_flg_map->find(thd_id)==thd_flg_map->end())
+		(*thd_flg_map)[thd_id]=true;
+}
+
+bool UnInitRead::HasWrite(MemMeta *meta,thread_t thd_id)
+{
+	if(memmeta_init_table_.find(meta)==memmeta_init_table_.end())
+		return false;
+	ThreadFlagMap *thd_flg_map=memmeta_init_table_[meta];
+	if(thd_flg_map->find(thd_id)==thd_flg_map->end())
+		return false;
+	return true;
+}
+
+DanglingPtr::DanglingPtr():filter_(NULL)
+{}
+
+DanglingPtr::~DanglingPtr()
+{
+	delete filter_;
+}
+
+inline void DanglingPtr::CreateFreedRegionFilter(Mutex *lock)
+{
+	filter_=new RegionFilter(new NullMutex);
+}
+
+inline void DanglingPtr::AddFreedRegion(address_t region_start,size_t region_size)
+{
+	DEBUG_ASSERT(region_size);
+	filter_->AddRegion(region_start,region_size);
+}
+
+inline bool DanglingPtr::InFreedRegion(address_t addr)
+{
+	return filter_->Filter(addr);
+}
+
+bool DanglingPtr::DeletePtr(MemAccess *mem_acc)
+{
+	std::string file_name=mem_acc->inst->GetFileName();
+	int line=mem_acc->inst->GetLine();
+	return del_ptr_set_.find(FilenameAndLineHash(file_name.c_str(),line))!=
+		del_ptr_set_.end();
+}
+
+inline thread_t DanglingPtr::Harmful(MemAccess *mem_acc1,MemAccess *mem_acc2)
+{
+	// One is `delete` operator and one is an access to the ptr.
+	if(DeletePtr(mem_acc1) && !DeletePtr(mem_acc2))
+		return mem_acc1->thd_id;
+	else if(DeletePtr(mem_acc2) && !DeletePtr(mem_acc1))
+		return mem_acc2->thd_id;
+	return 0;
+}
+
+bool DanglingPtr::LoadStaticInfo(std::string &file_name)
+{
+	std::fstream in(file_name,std::ios::in);
+	if(!in)
+		return false;
+	/* Suspicious `delete`operator.
+	   @format filename +' '+ lineno */
+	const char *delimit=" ",*fn,*ln;
+	fn=ln=NULL;
+	char buffer[50];
+	while(in.eof()) {
+		in.getline(buffer,50,'\n');
+		fn=strtok(delimit,buffer);
+		if(fn==NULL)
+			continue;
+		ln=strtok(delimit,NULL);
+		del_ptr_set_.insert(FilenameAndLineHash(fn,atoi(ln)));
+	}
+	in.close();
+	return true;
+}
+
+BufferOverflow::BufferOverflow()
+{}
+
+BufferOverflow::~BufferOverflow()
+{}
+
+inline bool BufferOverflow::BufferIndex(MemAccess *mem_acc)
+{
+	string file_name=mem_acc->inst->GetFileName();
+	int line=mem_acc->inst->GetLine();
+	return buf_idx_set_.find(FilenameAndLineHash(file_name.c_str(),line))!=
+		buf_idx_set_.end();
+}
+
+bool BufferOverflow::Harmful(MemAccess *mem_acc1,MemAccess *mem_acc2)
+{
+	return mem_acc1==RACE_EVENT_READ ? BufferIndex(mem_acc1):
+		BufferIndex(mem_acc2);
+}
+
+bool BufferOverflow::LoadStaticInfo(std::string &file_name)
+{
+	std::fstream in(file_name,std::ios::in);
+	if(!in)
+		return false;
+	/* Suspicious index access.
+	   @format filename +' '+ lineno */ 
+	const char *delimit=" ",*fn,*ln;
+	fn=ln=NULL;
+	char buffer[50];
+	while(in.eof()) {
+		in.getline(buffer,50,'\n');
+		fn=strtok(delimit,buffer);
+		if(fn==NULL)
+			continue;
+		ln=strtok(delimit,NULL);
+		buf_idx_set_.insert(FilenameAndLineHash(fn,atoi(ln)));
+	}
+	in.close();
+	return true;
+}
+
+MemBug::MemBug(Knob *knob):knob_(knob),null_ptr_deref_(NULL),uninit_read_(NULL),
+	dangling_ptr_(NULL),buffer_overflow_(NULL)
+{}
+
+MemBug::~MemBug()
+{
+	if(null_ptr_deref_)
+		delete null_ptr_deref_;
+	if(uninit_read_)
+		delete uninit_read_;
+	if(dangling_ptr_)
+		delete dangling_ptr_;
+	if(buffer_overflow_)
+		delete buffer_overflow_;
+}
+
+void MemBug::Register()
+{
+	knob_->RegisterStr("null_ptr_deref","file name of the static information"
+		" about the null pointer dereference analysis","0");
+	knob_->RegisterStr("dangling_ptr","file name of the static information"
+		" about the dangling pointer analysis","0");
+	knob_->RegisterStr("buffer_overflow","file name of the static information"
+		" about the buffer index","0");
+}
+
+void MemBug::Setup()
+{
+	InitializeNullPtrDeref();
+	InitializeUnInitRead();
+	InitializeDanglingPtr();
+	InitializeBufferOverflow();
+}
+
+inline void MemBug::InitializeNullPtrDeref()
+{
+	null_ptr_deref_=new NullPtrDeref;
+	std::string file_name=knob_->ValueStr("null_ptr_deref");
+	if(!null_ptr_deref_->LoadStaticInfo(file_name)) {
+		delete null_ptr_deref_;
+		null_ptr_deref_=NULL;
+	}
+}
+
+inline void MemBug::InitializeUnInitRead()
+{
+	uninit_read_=new UnInitRead;
+	uninit_read_->CreateGlobalRegionFilter(new NullMutex);
+}
+
+inline void MemBug::InitializeDanglingPtr()
+{
+	dangling_ptr_=new DanglingPtr;
+	std::string file_name=knob_->ValueStr("dangling_ptr");
+	if(!dangling_ptr_->LoadStaticInfo(file_name)) {
+		delete dangling_ptr_;
+		dangling_ptr_=NULL;
+	}
+}
+
+inline void MemBug::InitializeBufferOverflow()
+{
+	buffer_overflow_=new BufferOverflow;
+	std::string file_name=knob_->ValueStr("buffer_overflow");
+	if(!buffer_overflow_->LoadStaticInfo(file_name)) {
+		delete buffer_overflow_;
+		buffer_overflow_=NULL;
+	}
+}
+
+thread_t MemBug::ProcessHarmfulRace(MemMeta *tmp_mem_meta,MemAccess *tmp_mem_acc1,
+	MemAccess *tmp_mem_acc2)
+{
+	/* Null pointer dereference, uninitialized read and buffer overflow are both 
+	   composed a read and a write access. 
+	   Dangling pointer may be composed of two write accesses. */
+	if(tmp_mem_acc1->type==RACE_EVENT_WRITE && tmp_mem_acc2->type==RACE_EVENT_READ ||
+		tmp_mem_acc1->type==RACE_EVENT_READ && tmp_mem_acc2->type==RACE_EVENT_WRITE) {
+		// Null pointer dereference do not need meta
+		if(null_ptr_deref_ && null_ptr_deref_->Harmful(tmp_mem_acc1,tmp_mem_acc2)) {
+			return t1==RACE_EVENT_WRITE ? tid2:tid1;
+		}
+		// Always make the read access go through laster.
+		if(uninit_read_) {
+			MemMeta *mem_meta=new MemMeta(tmp_mem_meta->addr);
+			if(uninit_read_->Harmful(mem_meta,tmp_mem_acc1,tmp_mem_acc2))
+				return t1==RACE_EVENT_WRITE ? tid2:tid1;
+		}
+		// Always make the `delete` operator go through later. 
+		if(dangling_ptr_) {
+			return dangling_ptr_->Harmful(tmp_mem_acc1,tmp_mem_acc2);
+		}
+		// Always make the write changing the index go through later.
+		if(buffer_overflow_ && buffer_overflow_->Harmful(tmp_mem_acc1,tmp_mem_acc2)) {
+			return t1==RACE_EVENT_WRITE ? tid1:tid2;
+		}
+	} else if(tmp_mem_acc1->type==RACE_EVENT_WRITE && 
+		tmp_mem_acc2->type==RACE_EVENT_WRITE) {
+		if(dangling_ptr_) {
+			// Always make the `delete` operator go through later. 
+			return dangling_ptr_->Harmful(tmp_mem_acc1,tmp_mem_acc2);
+		}
+	}
+	return 0;
+}
+} //namespace race
---
+++ src/race/membug.h
@@ -0,0 +1,200 @@
+/**
+  * @file race/membug.h
+  * Define the concurrent memory bug.
+  * 
+  * Now, we are enabled to analyze four types concurrent memory bugs
+  * that can result in the program crash.
+  * 
+  * Null pointer deference
+  * Uninitialized read
+  * Dangling pointer
+  * Buffer overflow
+  */
+#ifndef __RACE_MEMBUG_H
+#define __RACE_MEMBUG_H
+
+#include "core/basictypes.h"
+#include <map>
+#include <vector>
+#include <tr1/unordered_set>
+#include "race/race.h"
+#include "core/filter.h"
+#include "core/sync.h"
+
+namespace race
+{
+static uint64 FilenameAndLineHash(char *file_name,int line) {
+	uint64 key=0;
+	while(*file_name)
+		key += *file_name++;
+	key += line;
+	return key;
+}
+
+// The abstract data for the memory meta info.
+class MemMeta {
+public:
+	typedef std::tr1::unordered_map<address_t,MemMeta *> MemTable;
+	explicit MemMeta(address_t a):addr(a) {}
+	~MemMeta() {}
+	address_t addr;
+};
+// The abstract data for the memory access info.
+class MemAccess {
+public:
+	MemAccess(thread_t tid,Inst *i,RaceEventType t):thd_id(tid),inst(i),type(t){}
+	~MemAccess() {}
+	thread_t thd_id;
+	Inst *inst;
+	RaceEventType type;
+};
+
+/**
+  * Null pointer dereference concurrent memory bug is composed of
+  * a wp and rp from the two different threads. wp writes a NULL
+  * to a shared pointer. rp reads pointer and conducts the pointer
+  * deference. 
+  */
+class NullPtrDeref {
+public:
+	NullPtrDeref();
+	~NullPtrDeref();
+	//
+	bool Harmful(MemAccess *mem_acc1,MemAccess *mem_acc2);
+	// Load the information about the null pointer read
+	bool LoadStaticInfo(std::string &path);
+protected:
+	typedef std::tr1::unordered_set<uint64> NullPtrSet;
+	// Write the NULL to the pointer or read the pointer by dereference
+	bool ValidPtrReadOrWrite(MemAccess *mem_acc);
+	/* Pointer read access within the condition expression. Like
+	   `if(ptr==NULL)` or `if(ptr)` or `if(!ptr)` etc. */
+	NullPtrSet null_ptrrd_set_; 
+	/* Pointer write access must contains NULL. Like 
+	   `ptr=NULL` etc. */
+	NullPtrSet null_ptrwr_set_;
+private:
+	DISALLOW_COPY_CONSTRUCTORS(NullPtrDeref);
+};
+
+/**
+  * Un-initialized memory read bug is composed of a read and a definition
+  * to the shared memory location.
+  */
+class UnInitRead {
+public:
+	UnInitRead();
+	~UnInitRead();
+	//
+	bool Harmful(MemMeta *meta,MemAccess *mem_acc1,MemAccess *mem_acc2);
+	void CreateGlobalRegionFilter(Mutex *lock);
+	void AddGlobalRegion(address_t region_start,size_t region_size);
+protected:
+	typedef std::map<thread_t,bool> ThreadFlagMap;
+	typedef std::map<MemMeta*,ThreadFlagMap *> MemMetaInitTable;
+	bool InGlobalRegion(address_t addr);
+	// Set the first write to the heap memory location
+	void SetInitWrite(MemMeta *meta,thread_t thd_id);
+	// If the thread has been write initially
+	bool HasWrite(MemMeta *meta,thread_t thd_id);
+	RegionFilter *filter_;
+	MemMetaInitTable memmeta_init_table_;
+private:
+	DISALLOW_COPY_CONSTRUCTORS(UnInitRead);
+};
+
+/**
+  * Danlging pointer access bug is composed of an access to the heap memory
+  * location(write or read) and a delete operation to the heap memory.
+  * Danlging pointer access race is mainly incurred by the vptr in C++.
+  * When using the `delete` operator will clear the vptr and when accessing
+  * virtual function will access the vptr.
+  * In the C language program, `free` system function may not be access the
+  * shared memory location and may not conduct the data race.
+  */
+class DanglingPtr {
+public:
+	DanglingPtr();
+	~DanglingPtr();
+
+	// Return the thread executes the `delete` operator.
+	thread_t Harmful(MemAccess *mem_acc1,MemAccess *mem_acc2);
+	void CreateFreedRegionFilter(Mutex *lock);
+	void AddFreedRegion(address_t region_start,size_t region_size);
+	// Load the information about the `delete` operator.
+	bool LoadStaticInfo(std::string &file_name);
+protected:
+	typedef std::tr1::unordered_set<uint64> DeletePtrSet;
+	bool InFreedRegion(address_t addr);
+	bool DeletePtr(MemAccess *mem_acc);
+	RegionFilter *filter_;
+	DeletePtrSet del_ptr_set_;
+private:
+	DISALLOW_COPY_CONSTRUCTORS(DanglingPtr);
+};
+
+/**
+  * Buffer overflow memory bug is composed of a write access and a read
+  * access to the shared index variable. 
+  * We now only consider the shared memory buffer.
+  */
+class BufferOverflow {
+public:
+	BufferOverflow();
+	~BufferOverflow();
+	
+	bool Harmful(MemAccess *mem_acc1,MemAccess *mem_acc2);
+	// Load the information about the buffer index.
+	bool LoadStaticInfo(std::string &file_name);
+protected:
+	typedef std::tr1::unordered_set<uint64> BufferIndexSet;
+	bool BufferIndex(MemAccess *mem_acc);
+	BufferIndexSet buf_idx_set_;
+private:
+	DISALLOW_COPY_CONSTRUCTORS(BufferOverflow);
+};
+/**
+  * Manage all types of memory bugs
+  */
+class MemBug {
+public:
+	MemBug(Knob *knob);
+	~MemBug();
+
+	void Register();
+	void Setup();
+	MemMeta CreateMemMeta(address_t addr) {
+		return MemMeta(addr);
+	}
+	MemAccess CreateMemAccess(thread_t tid,Inst *i,RaceEventType t) {
+		return MemAccess(tid,i,t);
+	};
+	/* Process different type memory bugs and return the harmful thread
+	   that should go through later. */
+	thread_t ProcessHarmfulRace(MemMeta *tmp_mem_meta,MemAccess *tmp_mem_acc1,
+		MemAccess *tmp_mem_acc2);
+	/* Wrapper function of the `AddGlobalRegion` from the UnInitRead
+	   Only for null pointer dereference. */
+	void AddGlobalRegion(address_t region_start,size_t region_size) {
+		if(null_ptr_deref_)
+			null_ptr_deref_->AddGlobalRegion(region_start,region_size);
+	}
+
+protected:
+	void InitializeNullPtrDeref();
+	void InitializeUnInitRead();
+	void InitializeDanglingPtr();
+	void InitializeBufferOverflow();
+	Knob *knob_;
+
+	NullPtrDeref *null_ptr_deref_;
+	UnInitRead *uninit_read_;
+	DanglingPtr *dangling_ptr_;
+	BufferOverflow *buffer_overflow_;
+private:
+	DISALLOW_COPY_CONSTRUCTORS(MemBug);
+};
+
+} // namespace race
+
+#endif // __RACE_MEMBUG_H
